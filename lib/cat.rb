7e9af05f8495e5f3706f855a8eaa518fa4c6a95a
hopegipson <gipson.hope@gmail.com>
2020-07-14 20:51:31 -0600

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@catall = []
+
+  def initialize (name, owner = self)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@catall << self
+  end
+
+  def self.all
+    @@catall
+  end
+
+
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
+
+  @@dogall = []
+
+  def initialize (name, owner = self)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@dogall << self
+  end
+
+  def self.all
+    @@dogall
+  end
+
 end
