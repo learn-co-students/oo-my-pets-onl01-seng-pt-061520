da7ff169ee4601fd43a375149a8252e7b7733bfb
Celina Jackson <celinanjackson@gmail.com>
2020-08-10 20:49:10 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+ attr_accessor :mood
+ attr_reader :name
+
+ @@cats = []
+
+ def initialize(name, owner)
+   @name = name
+   @mood = "nervous"
+   @owner = owner
+ end
+
 end
